7865e64e5a4f994443df6eb8f4c3b9777f45c41d
ianmac15 <3thita@gmail.com>
2022-08-23 22:43:43 +0300

    1

--- productapp.php
+++ productapp.php
@@ -82,10 +82,10 @@ class ControllerExtensionModuleProductapp extends Controller {
         $data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
-        $data['test'] = "test";
+        
 
 
-        $this->response->setOutput($this->load->view('extension/module/productapp'), $data); 
+        $this->response->setOutput($this->load->view('extension/module/productapp', $data)); 
     }
 
 
