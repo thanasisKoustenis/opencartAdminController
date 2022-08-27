649043029e7bdffb40578cd815ab778210c1efd3
ianmac15 <44259297+ianmac15@users.noreply.github.com>
2022-08-27 14:43:09 +0300

    1

--- productapp.php
+++ productapp.php
@@ -83,9 +83,28 @@ class ControllerExtensionModuleProductapp extends Controller {
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
         
+        $data['breadcrumbs'] = array();
 
+        $data['breadcrumbs'][] = array(
+            'text'=> $this->language->get('text_home'),
+            'href'=> $this->url->link('common/dashboard' . '&user_token=' . $this->session->data['user_token'], true)
+        );
+
+        $data['breadcrumbs'][] = array(
+            'text'=> $this->language->get('heading_title'),
+            'href'=> $this->url->link('extension/module/productapp' . '&user_token=' . $this->session->data['user_token'], true)
+        );
+
+        $data['add'] = $this->url->link('extension/module/productapp/addProductForm' . '&user_token=' . $this->session->data['user_token'], true);
 
         $this->response->setOutput($this->load->view('extension/module/productapp', $data)); 
+    }
+
+    public function addProductForm() {
+
+        $data= array();
+
+        $this->response->setOutput($this->load->view('extension/module/productappAdd', $data)); 
     }
 
 
